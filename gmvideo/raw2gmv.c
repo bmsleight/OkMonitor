57ff3aaed21005621d3217bebdf8349b3a6b6a03
Brendan M. Sleight <bms.git@barwap.com>
2020-11-08 21:39:44 +0000

    #define sizes

--- gmvideo/raw2gmv.c
+++ gmvideo/raw2gmv.c
@@ -6,19 +6,21 @@
 #include <stdio.h> // stdin,stdout
 typedef unsigned char u8;
 typedef unsigned int u32;
+#define XVID 758
+#define YVID 1024
 int main(void) {
-  u8 o, to, tb, wb0[1024 * 758];
+  u8 o, to, tb, wb0[YVID * XVID];
   u32 x, y, xi, yi, c = 250, b = 120; // c=contrast, b=brightness
-  while (fread(wb0, 1024 * 758, 1, stdin))
-    for (y = 0; y < 1024; y++) {
+  while (fread(wb0, YVID * XVID, 1, stdin))
+    for (y = 0; y < YVID; y++) {
       xi = y;
       tb = 0;
-      for (x = 0; x < 758; x++) {
-        yi = 757 - x;
+      for (x = 0; x < XVID; x++) {
+        yi = XVID - 1 - x;
         o = x ^ y;
         to = (y >> 2 & 1 | o >> 1 & 2 | y << 1 & 4 | o << 2 & 8 | y << 4 & 16 |
               o << 5 & 32) -
-                 (wb0[yi * 1024 + xi] * 63 + b) / c >>
+                 (wb0[yi * YVID + xi] * 63 + b) / c >>
              8;
         tb = (tb >> 1) | (to & 128);
         if (7 == (x & 7)) {
